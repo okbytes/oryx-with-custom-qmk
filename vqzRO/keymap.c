2b3c1596d90ab33fa96037e5d3c8fc95fde92c14
Brett Flora <1brettf@gmail.com>
2025-07-02 20:40:45 -0400

    add custom shifted numbers

--- vqzRO/keymap.c
+++ vqzRO/keymap.c
@@ -14,17 +14,17 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLS,        
-    KC_MINUS,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,       
-    ALL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
-    KC_LEFT_SHIFT,  MT(MOD_LCTL, KC_Z),MT(MOD_LALT, KC_X),MT(MOD_LGUI, KC_C),KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
+    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLS,
+    KC_MINUS,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,
+    ALL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
+    KC_LEFT_SHIFT,  MT(MOD_LCTL, KC_Z),MT(MOD_LALT, KC_X),MT(MOD_LGUI, KC_C),KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT,
                                                     KC_BSPC,        MT(MOD_LGUI, KC_TAB),                                MT(MOD_RCTL, KC_ENTER),LT(1, KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
     KC_NO,          KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_AUDIO_VOL_DOWN,
     KC_NO,          LGUI(LSFT(KC_1)),LGUI(LSFT(KC_2)),LGUI(LSFT(KC_3)),LGUI(LSFT(KC_4)),LGUI(LSFT(KC_5)),                                LGUI(LSFT(KC_C)),KC_NO,          LALT(LGUI(KC_I)),KC_NO,          KC_NO,          KC_AUDIO_VOL_UP,
-    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_ENTER,       KC_TAB,         
-    KC_LEFT_SHIFT,  KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_NO,          KC_NO,                                          KC_NO,          KC_SPACE,       KC_NO,          KC_NO,          KC_NO,          KC_RIGHT_SHIFT, 
+    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_ENTER,       KC_TAB,
+    KC_LEFT_SHIFT,  KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_NO,          KC_NO,                                          KC_NO,          KC_SPACE,       KC_NO,          KC_NO,          KC_NO,          KC_RIGHT_SHIFT,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
@@ -92,7 +92,6 @@ bool rgb_matrix_indicators_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
@@ -102,4 +101,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+//
+// custom qmk
+//
 
+// Shift 2 is {
+const key_override_t two_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_LCBR);
+// Shift 3 is (
+const key_override_t three_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_3, KC_LPRN);
+// Shift 4 is [
+const key_override_t four_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_LBRC);
+// Shift 5 is $
+const key_override_t five_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_5, KC_DLR);
+// Shift 6 is *
+const key_override_t six_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_6, KC_ASTR);
+// Shift 7 is ]
+const key_override_t sever_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_7, KC_RBRC);
+// Shift 8 is )
+const key_override_t eight_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_8, KC_RPRN);
+// Shift 9 is }
+const key_override_t nine_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_9, KC_RCBR);
+// Shift 0 is &
+const key_override_t zero_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_0, KC_AMPR);
+
+const key_override_t* key_overrides[] = {
+  &two_key_override,
+  &three_key_override,
+  &four_key_override,
+  &five_key_override,
+  &six_key_override,
+  &seven_key_override,
+  &eight_key_override,
+  &nine_key_override,
+  &zero_key_override,
+  NULL
+};
